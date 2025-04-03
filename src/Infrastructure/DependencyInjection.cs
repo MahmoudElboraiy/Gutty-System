e6fa2b6df175f6c6c7dfcfec4b0e9503bc88e27d
Amir <sagan.5050@gmail.com>
2025-04-03 08:42:43 +0200

    Init Clean Architecture

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,71 @@
+using System.Text;
+using Domain.Models.Identity;
+using Infrastructure.Data;
+using Infrastructure.JwtAuthentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var jwtSettings = new JwtSettings();
+
+        configuration.Bind(nameof(jwtSettings), jwtSettings);
+
+        services.AddSingleton(jwtSettings);
+
+        services
+            .AddIdentityCore<User>(o =>
+            {
+                o.User.RequireUniqueEmail = false;
+                o.SignIn.RequireConfirmedPhoneNumber = true;
+                o.SignIn.RequireConfirmedEmail = false;
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = true;
+                o.Password.RequireUppercase = true;
+                o.Password.RequiredLength = 8;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+
+        services
+            .AddAuthentication(cfg =>
+            {
+                cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                cfg.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddJwtBearer(options =>
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Key)
+                    ),
+                }
+            );
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
