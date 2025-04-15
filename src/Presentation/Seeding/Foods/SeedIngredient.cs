21869e5fe7665a8322b411adc7f59612c022ca83
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-04-15 07:19:51 +0200

    seed ingerdient

---
+++ src/Presentation/Seeding/Foods/SeedIngredient.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Models.Entities;
+using Domain.Models.Identity;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Presentation.Seeding.Foods
+{
+    public static class SeedIngredient
+    {
+        public static void Seed(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Ingredient>().HasData(
+              new Ingredient
+              {
+                  Id = 1,
+                  Name = "Flour",
+                  AmountNeeded = 5.0m,
+                  Stock = 100.0m
+              },
+              new Ingredient
+              {
+                  Id = 2,
+                  Name = "Sugar",
+                  AmountNeeded = 2.0m,
+                  Stock = 50.0m
+              },
+              new Ingredient
+              {
+                  Id = 3,
+                  Name = "Butter",
+                  AmountNeeded = 1.0m,
+                  Stock = 30.0m
+              }
+          );
+        }
+    }
+}
