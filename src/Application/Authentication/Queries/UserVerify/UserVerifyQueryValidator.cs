0aed9cb9127febbd22169988f61f85415f15c7a4
mahmoud-elboraiy <99480478+mahmoud-elboraiy@users.noreply.github.com>
2025-04-17 16:21:40 +0200

    Validation

---
+++ src/Application/Authentication/Queries/UserVerify/UserVerifyQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Authentication.Queries.UserVerify
+{
+    public class UserVerifyQueryValidator:AbstractValidator<UserVerifyQuery>
+    {
+        public UserVerifyQueryValidator()
+        {
+            RuleFor(u => u.UserId).NotEmpty().WithMessage("معرّف المستخدم مطلوب");
+        }
+    }
+}
---
+++ src/Application/Items/Queries/GetItems/GetItemsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Items.Queries.GetItems
+{
+    public class GetItemsQueryValidator:AbstractValidator<GetItemsQuery>
+    {
+        public GetItemsQueryValidator()
+        {
+            RuleFor(x => x.SearchText)
+                .MaximumLength(100).WithMessage("نص البحث لا يجب أن يتجاوز 100 حرف");
+        }
+    }
+}
